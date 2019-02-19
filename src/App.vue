0236abc7857a11c5bb4685a3478e9179d6e9c1da
jpelliza <jpelliza.gcba@gmail.com>
2019-02-19 18:03:31 -0300

    Tratando de entender la estructura (?)

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,104 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+            <!-- CONTENIDO -->
+    <main class="main-container" role="main" id="app">
+
+      <!-- BÚSCADOR -->
+      <div class="container">
+
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>Guía Digital de Patrimonios Culturales</h1>
+            <br>
+          </div>
+        </div>
+
+        <div class="row">
+          <form role="form">
+            <div class="col-xs-12">
+              <div class="form-group has-button">
+                <input type="text" class="form-control" placeholder="Buscar entre los 300 patrimonios...">
+                <button class="btn">
+                  <span class="glyphicon glyphicon-search"></span>
+                </button>
+              </div>
+            </div>
+              
+            <div class="col-xs-12 col-md-6">
+              <div class="form-group has-button">
+                <select class="form-control chosen-select" data-placeholder="Por categoría" v-model="categoriaSeleccionada">
+                  <option value="">Filtrar por categoría...</option>
+                  <option v-for="categoria in categorias" v-bind:value="categoria.id">{{ categoria.nombre }}</option>
+                </select>
+              </div>
+            </div>
+              
+            <div class="col-xs-12 col-md-6">
+              <div class="form-group has-button">
+                <select class="form-control chosen-select" data-placeholder="Por barrio" v-model="barrioSeleccionado">
+                  <option value="">Filtrar por barrio...</option>
+                  <option v-for="barrio in barrios" v-bind:value="barrio.id">{{ barrio.nombre }}</option>
+                </select>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <hr>
+            <br>
+          </div>
+        </div>
+
+        <div class="row row-flex">
+                              
+          <div class="col-xs-12 col-sm-6 col-md-4" v-for="bien in bienesFiltrados">
+            <div class="thumbnail">
+              <img src="src/img/thumb.png" alt="" width="100%" />
+              <div class="caption">
+                <h4><span class="label label-default">{{ categoriaPorId(bien.categoriaId) }}</span></h4>
+                <h3>{{ bien.denominacion }}</h3>
+                <p>{{ bien.sintesis }}</p>
+                <br>
+                <p>Barrio: {{ barrioPorId(bien.barrioId) }}</p>
+                <p>Antigüedad: {{ bien.epoca }}.</p>
+                <p><a v-bind:href="'bienes/' + bien.id" class="btn btn-primary" role="button">Ver ficha técnica</a></p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <div class="text-center">
+              <ul class="pagination">
+                <li class="disabled"><a href="#" class="glyphicon glyphicon-chevron-left"></a></li>
+                <li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#" class="glyphicon glyphicon-chevron-right"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+        
+    </main>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    name: 'app',
+    data() {
+        return {
+            title: ''
+        }
     }
-  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,84 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    categorias: [],
+    barrios: [],
+    bienes: [],
+    categoriaSeleccionada: "",
+    barrioSeleccionado: "",
+  },
+  mounted() {
+    axios.get("https://patrimoniosculturales-qa.gcba.gob.ar/api/categorias")
+      .then(response => {this.categorias = this.categoriasFormato(response.data) });
+    axios.get("https://patrimoniosculturales-qa.gcba.gob.ar/api/barrios")
+      .then(response => {this.barrios = this.barriosFormato(response.data) });
+    axios.get("https://patrimoniosculturales-qa.gcba.gob.ar/api/bienes")
+      .then(response => {this.bienes = response.data});
+  },
+  methods: {
+    barrioPorId: function(id) {
+      nombreDeBarrio = this.barrios.filter(function (barrio) {
+        return barrio.id == id;
+      });
+      if(nombreDeBarrio.length > 0) {
+        return nombreDeBarrio[0].nombre;
+      }
+      return "";
+    },
+    categoriaPorId: function(id) {
+      nombreDeCategoria = this.categorias.filter(function (categoria) {
+        return categoria.id == id;
+      });
+      if(nombreDeCategoria.length > 0) {
+        return nombreDeCategoria[0].nombre;
+      }
+      return "";
+    },
+    barriosFormato: function(data) {
+      for(barrio in data) {
+        data[barrio].nombre = data[barrio].nombre.charAt(0).toUpperCase() + data[barrio].nombre.slice(1).toLowerCase();
+      }
+      return data;
+    },
+    categoriasFormato: function(data) {
+      for(var categoria in data) {
+        data[categoria].nombre = data[categoria].nombre.charAt(0).toUpperCase() + data[categoria].nombre.slice(1).toLowerCase();
+      }
+      return data;
+    }
+  },
+  computed: {
+		bienesFiltrados: function() {
+			var vm = this;
+			var categoria = vm.categoriaSeleccionada;
+			var barrio = vm.barrioSeleccionado;
+			
+			if(categoria === "" && barrio === "") {
+				return vm.bienes;
+			}
+      else {
+        results = vm.bienes.filter(function(bien) {
+          if(categoria === "") {
+            return bien.barrioId === barrio;
+          }
+          if(barrio === "") {
+            return bien.categoriaId === categoria;
+          }
+					return bien.categoriaId === categoria && bien.barrioId === barrio;
+				});
+        if(results.length === 0) {
+          return [{"denominacion": "No se encontraron resultados", "localizacion": "", "contenido": "Modifique los parametros de los filtros para ver otras opciones."}]
+        }
+				return results.sort(function() { return 0.5 - Math.random() });
+			}
+    }
+  }
+});
